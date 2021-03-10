d3ce47c1b27c0c05a7e5406159ccbbd94acd6e71
ronsigter <ronchester.mendoza@gmail.com>
2021-03-10 10:30:11 +0800

    makes a util function to get hash

--- src/common/layouts/CoreLayout.js
+++ src/common/layouts/CoreLayout.js
@@ -3,19 +3,7 @@ import Header from '../components/Header'
 import SideBar from '../components/SideBar'
 import Player from '../components/Player'
 import Login from '../components/Login/Login'
-
-// Get the hash of the url
-const hash = window.location.hash
-  .substring(1)
-  .split('&')
-  .reduce(function (initial, item) {
-    if (item) {
-      var parts = item.split('=')
-      initial[parts[0]] = decodeURIComponent(parts[1])
-    }
-    return initial
-  }, {})
-window.location.hash = ''
+import { hash } from '../../utils/getHash'
 
 function CoreLayout({ children, history }) {
   const [token, setToken] = useState(null)
